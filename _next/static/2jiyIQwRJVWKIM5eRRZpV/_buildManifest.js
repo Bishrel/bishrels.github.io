16d16b5bca72759b12dafb48640655db413ce50f
Bishrel E <bishrele@Bishrels-MacBook-Air.local>
2022-04-18 18:51:04 +0800

    Updates

--- _next/static/2jiyIQwRJVWKIM5eRRZpV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1f5af17db5838adb.css","static/chunks/pages/index-013c7eb601cb730a.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
